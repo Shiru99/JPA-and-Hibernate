289d6381802bbee67e0be882b19b339700227005
Shiru99 <shriram.ghadge1999@gmail.com>
2021-07-12 21:48:50 +0530

    where, order by, like, between

--- jpql/src/main/java/io/summer/JPQL.java
+++ jpql/src/main/java/io/summer/JPQL.java
@@ -1,11 +1,9 @@
 package io.summer;
 
-import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 
@@ -36,6 +34,31 @@ public class JPQL
         for (Employee e : resultList) {
             System.out.println(e);
         }
+
+        System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+
+        String qString1 =   "SELECT e.name FROM Employee e"+
+                            "   WHERE e.name LIKE 'J%'";
+
+        TypedQuery<String> query1 = entityManager.createQuery(qString1,String.class);
+        List<String> resultList1 = query1.getResultList();
+
+        for (String str : resultList1) {
+            System.out.println(str);
+        }
+
+        System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+
+        String qString2 =   "SELECT e FROM Employee e"+
+                            "   WHERE e.id between 1 and 2";
+
+        TypedQuery<Employee> query2 = entityManager.createQuery(qString2,Employee.class);
+        List<Employee> resultList2 = query2.getResultList();
+
+        for (Employee employee : resultList2) {
+            System.out.println(employee);
+        }
+
 
         entityManager.close();
         emFactory.close();
