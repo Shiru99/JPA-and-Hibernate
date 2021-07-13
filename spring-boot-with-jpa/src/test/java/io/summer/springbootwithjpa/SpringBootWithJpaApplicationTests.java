e980e27428a0749b30a11bb58822e093d15dc1e0
Shiru99 <shriram.ghadge1999@gmail.com>
2021-07-13 10:09:11 +0530

    initiated

---
+++ spring-boot-with-jpa/src/test/java/io/summer/springbootwithjpa/SpringBootWithJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.summer.springbootwithjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
