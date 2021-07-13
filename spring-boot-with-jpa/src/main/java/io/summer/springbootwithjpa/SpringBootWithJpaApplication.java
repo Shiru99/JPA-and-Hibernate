6f884606021c19fcdc40a6366fbcdf16eed36537
Shiru99 <shriram.ghadge1999@gmail.com>
2021-07-13 14:37:35 +0530

    shared PersistenceContext

--- spring-boot-with-jpa/src/main/java/io/summer/springbootwithjpa/SpringBootWithJpaApplication.java
+++ spring-boot-with-jpa/src/main/java/io/summer/springbootwithjpa/SpringBootWithJpaApplication.java
@@ -6,6 +6,8 @@ import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
 import javax.persistence.PersistenceUnit;
 
 import org.springframework.boot.SpringApplication;
@@ -23,15 +25,23 @@ public class SpringBootWithJpaApplication {
 		SpringApplication.run(SpringBootWithJpaApplication.class, args);
 	}
 	
-	@PersistenceUnit
-	private EntityManagerFactory emf;
+	// M-1
+	// @PersistenceUnit
+	// private EntityManagerFactory emf;
+
+	// M-2
+	// @PersistenceContext(type = PersistenceContextType.EXTENDED) // ensures - Thread-Safety
+	@PersistenceContext
+	private EntityManager entityManager;
 
 	@PostConstruct 					/* Run Custom Method ==> PostConstruct */
 	public void start() 
 	{
 		Employee employee = new Employee("James","3456789012",new Date(),EmployeeType.INTERN);
 
-		EntityManager entityManager = emf.createEntityManager();
+		/*	M-1 
+
+		EntityManager entityManager = emf.createEntityManager();	
         EntityTransaction entityTransaction =  entityManager.getTransaction();
 
 		entityTransaction.begin();
@@ -40,6 +50,29 @@ public class SpringBootWithJpaApplication {
 
 		entityManager.close();
 		emf.close();
+
+		*/
+
+		/* M-2 
+
+			This is shared PersistenceContext (need to be thread-safe, multiple processes using same PersistenceContext can turn into unpredictable results)
+
+			1. Error : Not allowed to create transaction on shared EntityManager - use Spring transactions or EJB CMT instead ==> PersistenceContextType.EXTENDED
+
+			BUT we can perform read operation
+		*/
+
+		// NO transaction allowed unless ensures - Thread-Safety
+		// EntityTransaction entityTransaction =  entityManager.getTransaction();
+		// entityTransaction.begin();
+            // entityManager.persist(employee);
+		// entityTransaction.commit();
+		// entityManager.flush();
+
+
+		// Reading allowed
+		Employee e = entityManager.find(Employee.class, 1);
+		System.out.println(e);
+
 	}
-	
 }
