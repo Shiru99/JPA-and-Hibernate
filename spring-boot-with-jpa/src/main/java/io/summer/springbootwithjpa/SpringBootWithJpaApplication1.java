5e165415496346c2a71e05a39aeba8694d5bf25b
Shiru99 <shriram.ghadge1999@gmail.com>
2021-07-13 15:36:03 +0530

    Declarative Transaction

--- spring-boot-with-jpa/src/main/java/io/summer/springbootwithjpa/SpringBootWithJpaApplication1.java
+++ spring-boot-with-jpa/src/main/java/io/summer/springbootwithjpa/SpringBootWithJpaApplication1.java
@@ -1,5 +1,6 @@
 package io.summer.springbootwithjpa;
 
+import java.sql.SQLException;
 import java.util.Date;
 import java.util.Optional;
 
@@ -9,12 +10,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.Transactional;
 
 import io.summer.model.Employee;
 import io.summer.model.EmployeeType;
 
 @SpringBootApplication
 @EntityScan(basePackages = "io.summer")
+@EnableTransactionManagement
 public class SpringBootWithJpaApplication1 {
 
 	public static void main(String[] args) {
@@ -28,14 +32,42 @@ public class SpringBootWithJpaApplication1 {
 	public void start() 
 	{
 		Employee employee = new Employee("James","3456789012",new Date(),EmployeeType.INTERN);
-
 		employeeRepository.save(employee);
 
-		Optional<Employee> e = employeeRepository.findById(1);
+		Optional<Employee> e = employeeRepository.findById(2);
 		if(e.isPresent())
 			System.out.println(e);
 		else
 			System.out.println("NOT Available");
 
+
+		Optional<Employee> e2 = employeeRepository.findById(2);
+		if(e2.isPresent()){
+			updateEmployee(e2.get());
+		}
 	}
+
+	@Transactional(rollbackFor = SQLException.class, noRollbackFor = ArithmeticException.class)
+	private void updateEmployee(Employee e) {
+
+		// Get Transaction
+		// Start Transaction
+		e.setLastUpdateAt(new Date());
+		employeeRepository.save(e);
+		// End Transaction
+		// Handle Rollback
+	}
+	/* 
+
+		* Transaction -
+				
+			A database transaction is a sequence of actions that are treated as a single unit of work. These actions should either complete entirely or take no effect at all.
+
+
+		* Rollback -
+				
+			A rollback is the operation of restoring a database to a previous state by canceling a specific transaction or transaction set
+
+			BY default, for any exception happens spring auto-rollbacks
+	*/
 }
